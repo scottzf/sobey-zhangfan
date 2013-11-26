26d2528dcd53644673514ea36d4d3c208ad1d521
刘凯 <kai8406@gmail.com>
2013-11-26 10:33:37 +0800

    完善CMDBuildConstants 的说明.

--- sobey-projects/cmdbuild/src/main/java/com/sobey/cmdbuild/constants/CMDBuildConstants.java
+++ sobey-projects/cmdbuild/src/main/java/com/sobey/cmdbuild/constants/CMDBuildConstants.java
@@ -3,23 +3,30 @@ package com.sobey.cmdbuild.constants;
 /**
  * CMDBuild 常量定义.
  * 
+ * <pre>
+ * "A" 活跃		最新数据
+ * "U" 更新		数据更新前的数据
+ * "N" 非活跃	逻辑删除的数据
+ * 
+ * <pre>
+ * 
  * @author Administrator
  * 
  */
 public class CMDBuildConstants {
 
 	/**
-	 * "A" 活跃 65
+	 * "A" 活跃
 	 */
 	public static final Character STATUS_ACTIVE = 'A';
 
 	/**
-	 * "U" 更新 85
+	 * "U" 更新
 	 */
 	public static final Character STATUS_UPDATE = 'U';
 
 	/**
-	 * "N" 非活跃(删除) 78
+	 * "N" 非活跃
 	 */
 	public static final Character STATUS_NON_ACTIVE = 'N';
 
