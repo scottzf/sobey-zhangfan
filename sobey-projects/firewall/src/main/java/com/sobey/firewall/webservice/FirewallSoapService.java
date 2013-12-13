3facd9f52a9c53b8804773830695059280c285bc
刘凯 <kai8406@gmail.com>
2013-12-13 13:45:09 +0800

    完成firewall上的接口编写

--- sobey-projects/firewall/src/main/java/com/sobey/firewall/webservice/FirewallSoapService.java
+++ sobey-projects/firewall/src/main/java/com/sobey/firewall/webservice/FirewallSoapService.java
@@ -1,8 +1,14 @@
 package com.sobey.firewall.webservice;
 
+import java.util.List;
+
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 import com.sobey.firewall.constans.WsConstants;
+import com.sobey.firewall.webservice.response.dto.EIPParameter;
+import com.sobey.firewall.webservice.response.dto.VPNUserParameter;
+import com.sobey.firewall.webservice.response.result.WSResult;
 
 /**
  * 防火墙firewall对外暴露的唯一的webservice接口.
@@ -12,5 +18,56 @@ import com.sobey.firewall.constans.WsConstants;
  */
 @WebService(name = "FirewallSoapService", targetNamespace = WsConstants.NS)
 public interface FirewallSoapService {
+
+	/**
+	 * 在防火墙上执行脚本，创建EIP
+	 * 
+	 * @param parameter
+	 *            {@link EIPParameter}
+	 * @param allPolicies
+	 *            所有EIP的映射策略.
+	 * @return
+	 */
+	WSResult createEIPByFirewall(@WebParam(name = "EIPParameter") EIPParameter parameter,
+			@WebParam(name = "allPolicies") List<String> allPolicies);
+
+	/**
+	 * 在防火墙上执行脚本，删除EIP
+	 * 
+	 * @param parameter
+	 *            {@link EIPParameter}
+	 * @param allPolicies
+	 *            所有EIP的映射策略.
+	 * @return
+	 */
+	WSResult deleteEIPByFirewall(@WebParam(name = "EIPParameter") EIPParameter parameter,
+			@WebParam(name = "allPolicies") List<String> allPolicies);
+
+	/**
+	 * 在防火墙上执行脚本，创建VPN User
+	 * 
+	 * @param parameter
+	 *            {@link VPNUserParameter }
+	 * @return
+	 */
+	WSResult createVPNUserByFirewall(@WebParam(name = "VPNUserParameter") VPNUserParameter parameter);
+
+	/**
+	 * 在防火墙上执行脚本，删除VPN User
+	 * 
+	 * @param parameter
+	 *            {@link VPNUserParameter }
+	 * @return
+	 */
+	WSResult deleteVPNUserByFirewall(@WebParam(name = "VPNUserParameter") VPNUserParameter parameter);
+
+	/**
+	 * 在防火墙上执行脚本，为VPUN用户组新增或删除可访问段.
+	 * 
+	 * @param parameter
+	 *            {@link VPNUserParameter }
+	 * @return
+	 */
+	WSResult changeVPNUserAccesssAddressByFirewall(@WebParam(name = "VPNUserParameter") VPNUserParameter parameter);
 
 }
--- sobey-projects/firewall/src/main/java/com/sobey/firewall/webservice/FirewallSoapServiceImpl.java
+++ sobey-projects/firewall/src/main/java/com/sobey/firewall/webservice/FirewallSoapServiceImpl.java
@@ -1,10 +1,92 @@
 package com.sobey.firewall.webservice;
 
+import java.util.List;
+
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
+import com.sobey.core.utils.PropertiesLoader;
+import com.sobey.core.utils.TelnetUtil;
 import com.sobey.firewall.constans.WsConstants;
+import com.sobey.firewall.script.GenerateScript;
+import com.sobey.firewall.webservice.response.dto.EIPParameter;
+import com.sobey.firewall.webservice.response.dto.VPNUserParameter;
+import com.sobey.firewall.webservice.response.result.WSResult;
 
 @WebService(serviceName = "FirewallSoapService", endpointInterface = "com.sobey.firewall.webservice.FirewallSoapService", targetNamespace = WsConstants.NS)
 public class FirewallSoapServiceImpl implements FirewallSoapService {
+
+	/**
+	 * 加载applicationContext.propertie文件
+	 */
+	protected static PropertiesLoader FIREWALL_LOADER = new PropertiesLoader("classpath:/firewall.properties");
+
+	/* 防火墙登录 */
+	protected static final String FIREWALL_IP = FIREWALL_LOADER.getProperty("FIREWALL_IP");
+	protected static final String FIREWALL_USERNAME = FIREWALL_LOADER.getProperty("FIREWALL_USERNAME");
+	protected static final String FIREWALL_PASSWORD = FIREWALL_LOADER.getProperty("FIREWALL_PASSWORD");
+
+	@Override
+	public WSResult createEIPByFirewall(@WebParam(name = "EIPParameter") EIPParameter parameter,
+			@WebParam(name = "allPolicies") List<String> allPolicies) {
+
+		String command = GenerateScript.generateCreateEIPScript(parameter, allPolicies);
+
+		TelnetUtil.execCommand(FIREWALL_IP, FIREWALL_USERNAME, FIREWALL_PASSWORD, command);
+
+		// TODO 缺少针对返回字符串解析是否执行成功的判断.
+
+		return new WSResult();
+	}
+
+	@Override
+	public WSResult deleteEIPByFirewall(@WebParam(name = "EIPParameter") EIPParameter parameter,
+			@WebParam(name = "allPolicies") List<String> allPolicies) {
+
+		String command = GenerateScript.generateDeleteEIPScript(parameter, allPolicies);
+
+		TelnetUtil.execCommand(FIREWALL_IP, FIREWALL_USERNAME, FIREWALL_PASSWORD, command);
+
+		// TODO 缺少针对返回字符串解析是否执行成功的判断.
+
+		return new WSResult();
+	}
+
+	@Override
+	public WSResult createVPNUserByFirewall(@WebParam(name = "VPNUserParameter") VPNUserParameter parameter) {
+
+		String command = GenerateScript.generateCreateVPNUserScript(parameter);
+
+		TelnetUtil.execCommand(FIREWALL_IP, FIREWALL_USERNAME, FIREWALL_PASSWORD, command);
+
+		// TODO 缺少针对返回字符串解析是否执行成功的判断.
+
+		return new WSResult();
+	}
+
+	@Override
+	public WSResult deleteVPNUserByFirewall(@WebParam(name = "VPNUserParameter") VPNUserParameter parameter) {
+
+		String command = GenerateScript.generateDeleteVPNUserScript(parameter);
+
+		TelnetUtil.execCommand(FIREWALL_IP, FIREWALL_USERNAME, FIREWALL_PASSWORD, command);
+
+		// TODO 缺少针对返回字符串解析是否执行成功的判断.
+
+		return new WSResult();
+	}
+
+	@Override
+	public WSResult changeVPNUserAccesssAddressByFirewall(
+			@WebParam(name = "VPNUserParameter") VPNUserParameter parameter) {
+
+		String command = GenerateScript.generateChangeAccesssAddressIntoVPNUserScript(parameter);
+
+		TelnetUtil.execCommand(FIREWALL_IP, FIREWALL_USERNAME, FIREWALL_PASSWORD, command);
+
+		// TODO 缺少针对返回字符串解析是否执行成功的判断.
+
+		return new WSResult();
+	}
 
 }
