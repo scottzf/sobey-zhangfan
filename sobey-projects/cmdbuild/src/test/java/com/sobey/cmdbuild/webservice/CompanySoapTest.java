25e5cb648266c797ecad8597f5d64b7871228ae9
刘凯 <kai8406@gmail.com>
2014-01-13 11:36:33 +0800

    干掉多余的注释。

--- sobey-projects/cmdbuild/src/test/java/com/sobey/cmdbuild/webservice/CompanySoapTest.java
+++ sobey-projects/cmdbuild/src/test/java/com/sobey/cmdbuild/webservice/CompanySoapTest.java
@@ -97,12 +97,6 @@ public class CompanySoapTest extends BaseFunctionalTestCase {
 
 		Map<String, Object> searchParams = new HashMap<String, Object>();
 
-		/**
-		 * TODO 查询有问题.设置 status = 'A' , 但是传递到下个方法后转换成了status = 65 , 存疑. <br>
-		 * 将该参数放置在最后一步的分页查询还是OK的.
-		 */
-		// searchParams.put("EQ_status", CMDBuildConstants.STATUS_ACTIVE);
-
 		PaginationResult<CompanyDTO> result = cmdbuildSoapService.getCompanyPagination(searchParams, 1, 10);
 
 		assertNotNull(result.getGetTotalElements());
