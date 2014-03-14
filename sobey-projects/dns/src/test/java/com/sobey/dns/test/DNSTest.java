36c0a2f54e47d167b71ea004687b95a09cd1db1f
刘凯 <kai8406@gmail.com>
2014-03-14 16:21:33 +0800

    重构DNS模块,采用junit测试.

---
+++ sobey-projects/dns/src/test/java/com/sobey/dns/test/DNSTest.java
@@ -0,0 +1,40 @@
+package com.sobey.dns.test;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.sobey.dns.data.TestData;
+import com.sobey.dns.service.NitroService;
+import com.sobey.dns.webservice.response.dto.DNSParameter;
+
+/**
+ * DNS单元测试.
+ * 
+ * @author Administrator
+ * 
+ */
+@ContextConfiguration({ "classpath:applicationContext.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class DNSTest extends TestCase {
+
+	@Autowired
+	private NitroService service;
+
+	@Test
+	public void createDns() {
+		DNSParameter parameter = TestData.randomDNSParameter();
+		assertTrue(service.createDns(parameter));
+	}
+
+	@Test
+	public void deleteDns() {
+		DNSParameter parameter = TestData.randomDNSParameter();
+		assertTrue(service.deleteDns(parameter));
+	}
+
+}
