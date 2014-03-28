1fa382d222732f10bc04a7154f034d0a79841bf2
刘凯 <kai8406@gmail.com>
2014-03-28 15:39:38 +0800

    rename
    将公共属性放在接口中.

--- sobey-projects/switch/src/test/java/com/sobey/switches/PbulicProperties.java
+++ sobey-projects/switch/src/test/java/com/sobey/switches/PbulicProperties.java
@@ -3,27 +3,27 @@ package com.sobey.switches;
 import com.sobey.core.utils.PropertiesLoader;
 
 /**
- * 配置文件导入的抽象类
+ * 配置文件导入的接口
  * 
  * @author Administrator
  * 
  */
-public abstract class PropertiesAbstract {
+public interface PbulicProperties {
 
 	/**
 	 * 加载applicationContext.propertie文件
 	 */
-	protected static PropertiesLoader CORE_LOADER = new PropertiesLoader("classpath:/Core.properties");
-	protected static PropertiesLoader ACCESS_LOADER = new PropertiesLoader("classpath:/Access.properties");
+	static PropertiesLoader CORE_LOADER = new PropertiesLoader("classpath:/Core.properties");
+	static PropertiesLoader ACCESS_LOADER = new PropertiesLoader("classpath:/Access.properties");
 
 	/* 核心交换机 */
-	protected static final String CORE_IP = CORE_LOADER.getProperty("CORE_IP");
-	protected static final String CORE_USERNAME = CORE_LOADER.getProperty("CORE_USERNAME");
-	protected static final String CORE_PASSWORD = CORE_LOADER.getProperty("CORE_PASSWORD");
+	static final String CORE_IP = CORE_LOADER.getProperty("CORE_IP");
+	static final String CORE_USERNAME = CORE_LOADER.getProperty("CORE_USERNAME");
+	static final String CORE_PASSWORD = CORE_LOADER.getProperty("CORE_PASSWORD");
 
 	/* 接入层交换机,可能有数量不定的接入层交换机,待后续优化 */
-	protected static final String ACCESS_IP = ACCESS_LOADER.getProperty("ACCESS_IP");
-	protected static final String ACCESS_USERNAME = ACCESS_LOADER.getProperty("ACCESS_USERNAME");
-	protected static final String ACCESS_PASSWORD = ACCESS_LOADER.getProperty("ACCESS_PASSWORD");
+	static final String ACCESS_IP = ACCESS_LOADER.getProperty("ACCESS_IP");
+	static final String ACCESS_USERNAME = ACCESS_LOADER.getProperty("ACCESS_USERNAME");
+	static final String ACCESS_PASSWORD = ACCESS_LOADER.getProperty("ACCESS_PASSWORD");
 
 }
