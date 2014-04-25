8f1a3fd250a886c2530cae6799a25ca4f4d121fb
刘凯 <kai8406@gmail.com>
2014-04-25 16:33:51 +0800

    新建DNS和loadbalancer测试数据类

---
+++ sobey-projects/cmop-api/src/test/java/com/sobey/api/data/DNSTestData.java
@@ -0,0 +1,67 @@
+package com.sobey.api.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.sobey.generate.dns.DNSParameter;
+import com.sobey.generate.dns.DNSPolicyParameter;
+import com.sobey.generate.dns.DNSPublicIPParameter;
+
+public class DNSTestData {
+
+	public static DNSParameter randomDNSParameter() {
+
+		List<DNSPublicIPParameter> publicIPs = Lists.newArrayList();
+		List<DNSPolicyParameter> policyParameters = new ArrayList<DNSPolicyParameter>();
+		List<DNSPolicyParameter> policyParameters2 = new ArrayList<DNSPolicyParameter>();
+
+		DNSPolicyParameter policyParameter = new DNSPolicyParameter();
+		policyParameter.setProtocolText("tcp");
+		policyParameter.setSourcePort(8080);
+		policyParameter.setTargetPort(8080);
+		policyParameters.add(policyParameter);
+
+		DNSPolicyParameter policyParameter2 = new DNSPolicyParameter();
+		policyParameter2.setProtocolText("tcp");
+		policyParameter2.setSourcePort(80);
+		policyParameter2.setTargetPort(80);
+
+		policyParameters.add(policyParameter2);
+
+		DNSPublicIPParameter ipParameter = new DNSPublicIPParameter();
+
+		ipParameter.setIpaddress("113.142.30.109");
+		ipParameter.getPolicyParameters().addAll(policyParameters);
+
+		publicIPs.add(ipParameter);
+
+		DNSPolicyParameter policyParameter3 = new DNSPolicyParameter();
+		policyParameter3.setProtocolText("tcp");
+		policyParameter3.setSourcePort(8080);
+		policyParameter3.setTargetPort(8080);
+		policyParameters2.add(policyParameter3);
+
+		DNSPolicyParameter policyParameter4 = new DNSPolicyParameter();
+		policyParameter4.setProtocolText("tcp");
+		policyParameter4.setSourcePort(80);
+		policyParameter4.setTargetPort(80);
+
+		policyParameters2.add(policyParameter4);
+
+		DNSPublicIPParameter ipParameter2 = new DNSPublicIPParameter();
+
+		ipParameter2.setIpaddress("113.200.74.140");
+		ipParameter2.getPolicyParameters().addAll(policyParameters2);
+
+		publicIPs.add(ipParameter2);
+
+		DNSParameter parameter = new DNSParameter();
+		parameter.setDomianName("mdnftp.sobeycache.com");
+		parameter.setDomianType("gslb");
+		parameter.getPublicIPs().addAll(publicIPs);
+
+		return parameter;
+	}
+
+}
--- sobey-projects/cmop-api/src/test/java/com/sobey/api/test/DnsTest.java
+++ sobey-projects/cmop-api/src/test/java/com/sobey/api/test/DnsTest.java
@@ -1,8 +1,5 @@
 package com.sobey.api.test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import junit.framework.TestCase;
 
 import org.junit.Test;
@@ -11,11 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.google.common.collect.Lists;
+import com.sobey.api.data.DNSTestData;
 import com.sobey.api.service.DnsService;
-import com.sobey.generate.dns.DNSParameter;
-import com.sobey.generate.dns.DNSPolicyParameter;
-import com.sobey.generate.dns.DNSPublicIPParameter;
 
 /**
  * 针对Dns的测试,别忘了导入applicationContext-api.xml. 因为是单元测试,所以没走web.xml那块.因此需要手动指定配置文件.
@@ -30,68 +24,14 @@ public class DnsTest extends TestCase {
 	@Autowired
 	private DnsService service;
 
-	private DNSParameter getParameter() {
-
-		List<DNSPublicIPParameter> publicIPs = Lists.newArrayList();
-		List<DNSPolicyParameter> policyParameters = new ArrayList<DNSPolicyParameter>();
-		List<DNSPolicyParameter> policyParameters2 = new ArrayList<DNSPolicyParameter>();
-
-		DNSPolicyParameter policyParameter = new DNSPolicyParameter();
-		policyParameter.setProtocolText("tcp");
-		policyParameter.setSourcePort(8080);
-		policyParameter.setTargetPort(8080);
-		policyParameters.add(policyParameter);
-
-		DNSPolicyParameter policyParameter2 = new DNSPolicyParameter();
-		policyParameter2.setProtocolText("tcp");
-		policyParameter2.setSourcePort(80);
-		policyParameter2.setTargetPort(80);
-
-		policyParameters.add(policyParameter2);
-
-		DNSPublicIPParameter ipParameter = new DNSPublicIPParameter();
-
-		ipParameter.setIpaddress("113.142.30.109");
-		ipParameter.getPolicyParameters().addAll(policyParameters);
-
-		publicIPs.add(ipParameter);
-
-		DNSPolicyParameter policyParameter3 = new DNSPolicyParameter();
-		policyParameter3.setProtocolText("tcp");
-		policyParameter3.setSourcePort(8080);
-		policyParameter3.setTargetPort(8080);
-		policyParameters2.add(policyParameter3);
-
-		DNSPolicyParameter policyParameter4 = new DNSPolicyParameter();
-		policyParameter4.setProtocolText("tcp");
-		policyParameter4.setSourcePort(80);
-		policyParameter4.setTargetPort(80);
-
-		policyParameters2.add(policyParameter4);
-
-		DNSPublicIPParameter ipParameter2 = new DNSPublicIPParameter();
-
-		ipParameter2.setIpaddress("113.200.74.140");
-		ipParameter2.getPolicyParameters().addAll(policyParameters2);
-
-		publicIPs.add(ipParameter2);
-
-		DNSParameter parameter = new DNSParameter();
-		parameter.setDomianName("mdnftp.sobeycache.com");
-		parameter.setDomianType("gslb");
-		parameter.getPublicIPs().addAll(publicIPs);
-
-		return parameter;
-	}
-
 	@Test
 	public void createDNSTest() {
-		assertEquals(service.createDNS(getParameter()).getCode(), "0");
+		assertEquals(service.createDNS(DNSTestData.randomDNSParameter()).getCode(), "0");
 	}
 
-	// @Test
+	 @Test
 	public void deleteDNSTest() {
-		assertEquals(service.deleteDNS(getParameter()).getCode(), "0");
+		assertEquals(service.deleteDNS(DNSTestData.randomDNSParameter()).getCode(), "0");
 	}
 
 }
