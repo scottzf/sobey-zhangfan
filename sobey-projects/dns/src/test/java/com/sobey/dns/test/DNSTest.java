f61e8421ddec15053e22b1605bf37c590d602d14
刘凯 <kai8406@gmail.com>
2014-11-18 16:33:22 +0800

    通过netscarler的sdk获得同步的一些必要参数.

--- sobey-projects/dns/src/test/java/com/sobey/dns/test/DNSTest.java
+++ sobey-projects/dns/src/test/java/com/sobey/dns/test/DNSTest.java
@@ -8,6 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.citrix.netscaler.nitro.exception.nitro_exception;
+import com.citrix.netscaler.nitro.resource.config.lb.lbvserver;
+import com.citrix.netscaler.nitro.resource.config.lb.lbvserver_binding;
+import com.citrix.netscaler.nitro.service.nitro_service;
+import com.citrix.netscaler.nitro.service.nitro_service.OnerrorEnum;
+import com.sobey.core.utils.PropertiesLoader;
 import com.sobey.dns.data.TestData;
 import com.sobey.dns.service.DnsService;
 import com.sobey.dns.webservice.response.dto.DNSParameter;
@@ -22,6 +28,31 @@ import com.sobey.dns.webservice.response.dto.DNSParameter;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class DNSTest extends TestCase {
 
+	/**
+	 * 加载applicationContext.propertie文件
+	 */
+	private static PropertiesLoader DNS_LOADER = new PropertiesLoader("classpath:/dns.properties");
+
+	/**
+	 * IP
+	 */
+	private static final String DNS_IP = DNS_LOADER.getProperty("DNS_IP");
+
+	/**
+	 * Username
+	 */
+	private static final String DNS_USERNAME = DNS_LOADER.getProperty("DNS_USERNAME");
+
+	/**
+	 * password
+	 */
+	private static final String DNS_PASSWORD = DNS_LOADER.getProperty("DNS_PASSWORD");
+
+	/**
+	 * protocol
+	 */
+	private static final String DNS_PROTOCOL = DNS_LOADER.getProperty("DNS_PROTOCOL");
+
 	@Autowired
 	private DnsService service;
 
@@ -35,6 +66,79 @@ public class DNSTest extends TestCase {
 	public void deleteGSLB() {
 		DNSParameter parameter = TestData.randomDNSParameter();
 		assertTrue(service.deleteGSLB(parameter));
+	}
+
+	private nitro_service getnitro_service() {
+		nitro_service client = null;
+		try {
+			client = new nitro_service(DNS_IP, DNS_PROTOCOL);
+			client.set_credential(DNS_USERNAME, DNS_PASSWORD);
+			client.set_onerror(OnerrorEnum.CONTINUE);
+			client.set_warning(true);
+			client.set_certvalidation(false);
+			client.set_hostnameverification(false);
+			client.login();
+		} catch (nitro_exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // 创建nitro的session
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return client;
+	}
+
+	@Test
+	public void get_lbvserver() {
+
+		/**
+		 * 获得所有的ELB
+		 */
+		try {
+			lbvserver[] result = lbvserver.get(getnitro_service());
+			if (result != null) {
+				System.out.println("get_lbvserver result::length=" + result.length);
+				for (lbvserver lbvserver2 : result) {
+					System.out.println(lbvserver2.get_name());
+				}
+
+			} else {
+				System.out.println("Exception::get_lbvserver - Done");
+			}
+		} catch (nitro_exception e) {
+			System.out
+					.println("Exception::get_lbvserver::errorcode=" + e.getErrorCode() + ",message=" + e.getMessage());
+		} catch (Exception e) {
+			System.err.println("Exception::get_lbvserver::message=" + e);
+		}
+	}
+
+	@Test
+	public void getlbvserver_bindings() {
+
+		/**
+		 * 获得ELB的策略信息
+		 */
+		try {
+			lbvserver_binding obj = new lbvserver_binding();
+			obj.set_name("10.10.2.55-HTTP-80");
+			lbvserver_binding result = lbvserver_binding.get(getnitro_service(), "10.10.2.55-HTTP-80");
+			if (result.get_lbvserver_service_bindings() != null) {
+				System.out.println("getlbvserver_bindings result::length="
+						+ result.get_lbvserver_service_bindings().length);
+				for (int i = 0; i < result.get_lbvserver_service_bindings().length; i++) {
+					System.out.println("svc name " + result.get_lbvserver_service_bindings()[i].get_servicename());
+				}
+			} else {
+				System.out.println("getlbvserver_bindings - Done");
+			}
+		} catch (nitro_exception e) {
+			System.out.println("Exception::getlbvserver_bindings::errorcode=" + e.getErrorCode() + ",message="
+					+ e.getMessage());
+		} catch (Exception e) {
+			System.err.println("Exception::getlbvserver_bindings::message=" + e);
+		}
 	}
 
 }
--- sobey-projects/loadbalancer/src/test/java/com/sobey/loadbalancer/test/ElbTest.java
+++ sobey-projects/loadbalancer/src/test/java/com/sobey/loadbalancer/test/ElbTest.java
@@ -8,6 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.citrix.netscaler.nitro.exception.nitro_exception;
+import com.citrix.netscaler.nitro.resource.config.gslb.gslbservice;
+import com.citrix.netscaler.nitro.resource.config.gslb.gslbvserver;
+import com.citrix.netscaler.nitro.resource.config.gslb.gslbvserver_gslbservice_binding;
+import com.citrix.netscaler.nitro.service.nitro_service;
+import com.citrix.netscaler.nitro.service.nitro_service.OnerrorEnum;
+import com.sobey.core.utils.PropertiesLoader;
 import com.sobey.loadbalancer.data.TestData;
 import com.sobey.loadbalancer.service.LoadbalanceService;
 import com.sobey.loadbalancer.webservice.response.dto.ELBParameter;
@@ -22,8 +29,52 @@ import com.sobey.loadbalancer.webservice.response.dto.ELBParameter;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ElbTest extends TestCase {
 
+	/**
+	 * 加载applicationContext.propertie文件
+	 */
+	private static PropertiesLoader LOADBALANCER_LOADER = new PropertiesLoader("classpath:/loadbalancer.properties");
+
+	/**
+	 * IP
+	 */
+	private static final String LOADBALANCER_IP = LOADBALANCER_LOADER.getProperty("LOADBALANCER_IP");
+
+	/**
+	 * Username
+	 */
+	private static final String LOADBALANCER_USERNAME = LOADBALANCER_LOADER.getProperty("LOADBALANCER_USERNAME");
+
+	/**
+	 * password
+	 */
+	private static final String LOADBALANCER_PASSWORD = LOADBALANCER_LOADER.getProperty("LOADBALANCER_PASSWORD");
+
+	/**
+	 * protocol
+	 */
+	private static final String LOADBALANCER_PROTOCOL = LOADBALANCER_LOADER.getProperty("LOADBALANCER_PROTOCOL");
+
 	@Autowired
 	private LoadbalanceService service;
+
+	private nitro_service getnitro_service() {
+		nitro_service client = null;
+		try {
+			client = new nitro_service(LOADBALANCER_IP, LOADBALANCER_PROTOCOL);
+			client.set_credential(LOADBALANCER_USERNAME, LOADBALANCER_PASSWORD);
+			client.set_onerror(OnerrorEnum.CONTINUE);
+			client.set_warning(true);
+			client.set_certvalidation(false);
+			client.set_hostnameverification(false);
+			client.login();
+		} catch (nitro_exception e) {
+			e.printStackTrace();
+		} // 创建nitro的session
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return client;
+	}
 
 	@Test
 	public void createElb() {
@@ -35,6 +86,71 @@ public class ElbTest extends TestCase {
 	public void deleteElb() {
 		ELBParameter parameter = TestData.randomELBParameter();
 		assertTrue(service.deleteElb(parameter));
+	}
+
+	@Test
+	public void get_gslbvserver() {
+
+		/**
+		 * 获得GSLB的VS
+		 */
+
+		try {
+			gslbvserver[] result = gslbvserver.get(getnitro_service());
+			for (gslbvserver gslbvserver2 : result) {
+				System.out.println("get_gslbvserver - name= " + gslbvserver2.get_name() + ", servicetype= "
+						+ gslbvserver2.get_servicetype());
+			}
+		} catch (nitro_exception e) {
+			System.out.println("Exception::get_gslbvserver::errorcode=" + e.getErrorCode() + ",message="
+					+ e.getMessage());
+		} catch (Exception e) {
+			System.err.println("Exception::get_gslbvserver::message=" + e);
+		}
+	}
+
+	@Test
+	public void get_gslbservice() {
+
+		/**
+		 * 根据serviceName获得详情
+		 */
+		try {
+			gslbservice result = gslbservice.get(getnitro_service(), "113.142.30.14");
+			System.out.println("get_gslbservice - servicename= " + result.get_servicename() + ", servicetype= "
+					+ result.get_servicetype());
+		} catch (nitro_exception e) {
+			System.out.println("Exception::get_gslbservice::errorcode=" + e.getErrorCode() + ",message="
+					+ e.getMessage());
+		} catch (Exception e) {
+			System.err.println("Exception::get_gslbservice::message=" + e);
+		}
+	}
+
+	@Test
+	public void get_gslbvserver_service_binding() {
+
+		/**
+		 * 获得VS关联的service
+		 */
+
+		try {
+			gslbvserver_gslbservice_binding[] result = gslbvserver_gslbservice_binding.get(getnitro_service(),
+					"lztest.sobeycache.com");
+			if (result != null) {
+				for (int i = 0; i < result.length; i++) {
+					System.out.println("get_gslbvserver_service_binding - vserver name= " + result[i].get_name()
+							+ ", servicename= " + result[i].get_servicename());
+				}
+			} else {
+				System.out.println("Exception::get_gslbvserver_service_binding - Done");
+			}
+		} catch (nitro_exception e) {
+			System.out.println("Exception::get_gslbvserver_service_binding::errorcode=" + e.getErrorCode()
+					+ ",message=" + e.getMessage());
+		} catch (Exception e) {
+			System.err.println("Exception::get_gslbvserver_service_binding::message=" + e);
+		}
 	}
 
 }
