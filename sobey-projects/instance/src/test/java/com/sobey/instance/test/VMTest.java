63745babfe1eaacbefd5be793953d00affbca965
刘凯 <kai8406@gmail.com>
2014-12-19 10:30:49 +0800

    完善instance VM的单元测试.

--- sobey-projects/instance/src/test/java/com/sobey/instance/test/VMTest.java
+++ sobey-projects/instance/src/test/java/com/sobey/instance/test/VMTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.sobey.core.utils.MathsUtil;
 import com.sobey.instance.constans.DataCenterEnum;
 import com.sobey.instance.data.TestData;
 import com.sobey.instance.service.VMService;
@@ -24,17 +25,22 @@ import com.sobey.instance.webservice.response.dto.ReconfigVMParameter;
 import com.sobey.instance.webservice.response.dto.RelationVMParameter;
 import com.sobey.instance.webservice.response.dto.VMInfoDTO;
 import com.vmware.vim25.InvalidProperty;
+import com.vmware.vim25.ManagedObjectReference;
 import com.vmware.vim25.RuntimeFault;
 import com.vmware.vim25.VirtualDevice;
 import com.vmware.vim25.VirtualDisk;
 import com.vmware.vim25.VirtualEthernetCard;
 import com.vmware.vim25.VirtualHardware;
 import com.vmware.vim25.VirtualMachineConfigInfo;
+import com.vmware.vim25.VirtualMachineRuntimeInfo;
+import com.vmware.vim25.mo.Datastore;
 import com.vmware.vim25.mo.Folder;
+import com.vmware.vim25.mo.HostSystem;
 import com.vmware.vim25.mo.InventoryNavigator;
 import com.vmware.vim25.mo.ManagedEntity;
 import com.vmware.vim25.mo.ServiceInstance;
 import com.vmware.vim25.mo.VirtualMachine;
+import com.vmware.vim25.mo.util.MorUtil;
 
 @ContextConfiguration({ "classpath:applicationContext.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -44,8 +50,59 @@ public class VMTest extends TestCase {
 	private VMService service;
 
 	@Test
+	public void getVMHostRelationship() throws RemoteException, MalformedURLException {
+
+		ServiceInstance si = new ServiceInstance(new URL("https://172.20.0.19/sdk"), "administrator@vsphere.local",
+				"Newmed!@s0bey", true);
+		Folder rootFolder = si.getRootFolder();
+
+		System.out.println("\n============ Virtual Machines ============");
+		ManagedEntity[] vms = new InventoryNavigator(rootFolder).searchManagedEntities(new String[][] { {
+				"VirtualMachine", "name" }, }, true);
+
+		System.out.println(vms.length);
+		for (int i = 0; i < vms.length; i++) {
+			System.out.println("vm[" + i + "]=" + vms[i].getName());
+			VirtualMachineRuntimeInfo vmri = (VirtualMachineRuntimeInfo) ((VirtualMachine) vms[i]).getRuntime();
+			ManagedObjectReference host = vmri.getHost();
+			ManagedObjectReference managedObjectReference = new ManagedObjectReference();
+			managedObjectReference.setType("HostSystem");
+			managedObjectReference.setVal(host.getVal());
+			HostSystem hostSystem = (HostSystem) MorUtil.createExactManagedEntity(si.getServerConnection(),
+					managedObjectReference);
+			System.err.println(hostSystem.getName());
+		}
+
+	}
+
+	@Test
+	public void getHostVMRelationship() throws RemoteException, MalformedURLException {
+
+		ServiceInstance si = new ServiceInstance(new URL("https://172.20.0.19/sdk"), "administrator@vsphere.local",
+				"Newmed!@s0bey", true);
+		Folder rootFolder = si.getRootFolder();
+
+		System.out.println("\n============ Virtual Machines ============");
+		ManagedEntity[] hosts = new InventoryNavigator(rootFolder).searchManagedEntities(new String[][] { {
+				"HostSystem", "name" }, }, true);
+
+		for (int i = 0; i < hosts.length; i++) {
+			ManagedObjectReference managedObjectReference = new ManagedObjectReference();
+			managedObjectReference.setType("HostSystem");
+			managedObjectReference.setVal(hosts[i].getMOR().getVal());
+			HostSystem hostSystem = (HostSystem) MorUtil.createExactManagedEntity(si.getServerConnection(),
+					managedObjectReference);
+			for (ManagedEntity managedEntity : hostSystem.getVms()) {
+				System.out.println(managedEntity.getName());
+			}
+		}
+
+	}
+
+	@Test
 	public void PrintInventory() throws RemoteException, MalformedURLException {
-		ServiceInstance si = new ServiceInstance(new URL("https://10.10.2.20/sdk"), "root", "vmware", true);
+		ServiceInstance si = new ServiceInstance(new URL("https://172.20.0.19/sdk"), "administrator@vsphere.local",
+				"Newmed!@s0bey", true);
 		Folder rootFolder = si.getRootFolder();
 
 		System.out.println("============ Data Centers ============");
@@ -58,6 +115,7 @@ public class VMTest extends TestCase {
 		System.out.println("\n============ Virtual Machines ============");
 		ManagedEntity[] vms = new InventoryNavigator(rootFolder).searchManagedEntities(new String[][] { {
 				"VirtualMachine", "name" }, }, true);
+		System.out.println(vms.length);
 		for (int i = 0; i < vms.length; i++) {
 			System.out.println("vm[" + i + "]=" + vms[i].getName());
 		}
@@ -67,7 +125,6 @@ public class VMTest extends TestCase {
 				"HostSystem", "name" }, }, true);
 		for (int i = 0; i < hosts.length; i++) {
 			System.out.println("host[" + i + "]=" + hosts[i].getName());
-			System.out.println(hosts[i]);
 		}
 
 		System.out.println("\n============ ResourcePools ============");
@@ -84,7 +141,8 @@ public class VMTest extends TestCase {
 
 	@Test
 	public void getResourcesInfo() throws RemoteException, MalformedURLException {
-		ServiceInstance si = new ServiceInstance(new URL("https://10.10.2.20/sdk"), "root", "vmware", true);
+		ServiceInstance si = new ServiceInstance(new URL("https://172.20.0.19/sdk"), "administrator@vsphere.local",
+				"Newmed!@s0bey", true);
 		Folder rootFolder = si.getRootFolder();
 
 		HashMap<String, String> map = new HashMap<String, String>();
@@ -92,7 +150,7 @@ public class VMTest extends TestCase {
 		ManagedEntity[] resourcePools = new InventoryNavigator(rootFolder).searchManagedEntities(new String[][] {
 				{ "ResourcePool", "name" }, { "HostSystem", "name" } }, true);
 		for (int i = 0; i < resourcePools.length; i++) {
-			System.err.println(resourcePools[i].getMOR().get_value());
+			// System.err.println(resourcePools[i].getMOR().get_value());
 
 			if (i + 1 != resourcePools.length) {
 
@@ -103,7 +161,7 @@ public class VMTest extends TestCase {
 
 		for (Entry<String, String> entity : map.entrySet()) {
 			System.out.println(entity.getKey());
-			System.out.println(entity.getValue());
+			System.err.println(entity.getValue());
 		}
 
 		si.getServerConnection().logout();
@@ -112,13 +170,12 @@ public class VMTest extends TestCase {
 	@Test
 	public void VMInfo() throws InvalidProperty, RuntimeFault, RemoteException, MalformedURLException {
 
-		ServiceInstance si = new ServiceInstance(new URL("https://10.10.2.20/sdk"), "root", "vmware", true);
+		ServiceInstance si = new ServiceInstance(new URL("https://172.20.0.19/sdk"), "administrator@vsphere.local",
+				"Newmed!@s0bey", true);
 		Folder rootFolder = si.getRootFolder();
 
 		VirtualMachine myVM = (VirtualMachine) new InventoryNavigator(rootFolder).searchManagedEntity("VirtualMachine",
-		// "liukai02@sobey.com-10.10.101.5");
-		// "10.10.2.36_Netscaler`");
-				"DataONTAP31-34");
+				"172.20.1.64");
 
 		/**
 		 * VM是否关机只和status有关系,
@@ -140,11 +197,15 @@ public class VMTest extends TestCase {
 		System.out.println("============ 内存大小：" + vmHardware.getMemoryMB());
 		System.out.println("============ 网络适配器：" + myVM.getGuest().getNet());
 
+		for (Datastore datastore : myVM.getDatastores()) {
+			System.out.println("============ 存储器：" + datastore.getName());
+		}
+
 		VirtualDevice[] vmDevices = vmHardware.getDevice();
 		for (int i = 0; i < vmDevices.length; i++) {
 			if (vmDevices[i] instanceof VirtualDisk) {
 				VirtualDisk vmDisk = (VirtualDisk) vmDevices[i];
-				System.out.println("============ 存储大小" + vmDisk.getCapacityInKB());
+				System.out.println("============ 存储大小" + MathsUtil.div(vmDisk.getCapacityInKB(), 1048576));
 			}
 
 			if (vmDevices[i] instanceof VirtualEthernetCard) {
@@ -158,7 +219,7 @@ public class VMTest extends TestCase {
 
 	@Test
 	public void getVMInfoDTO() {
-		VMInfoDTO dto = service.getVMInfoDTO("10.10.2.36_Netscaler");
+		VMInfoDTO dto = service.getVMInfoDTO("172.20.1.64", DataCenterEnum.XA.toString());
 		System.out.println(dto);
 	}
 
