5d53b8fed24a5d36bd9bd143fc32dbffb4a4f698
刘凯 <kai8406@gmail.com>
2015-01-02 22:43:08 +0800

    增加电源操作的enum

---
+++ sobey-projects/instance/src/main/java/com/sobey/instance/constans/PowerOperationEnum.java
@@ -0,0 +1,13 @@
+package com.sobey.instance.constans;
+
+/**
+ * 针对vcenter中电源的操作.
+ * 
+ * @author Administrator
+ * 
+ */
+public enum PowerOperationEnum {
+
+	poweroff, poweron, reboot, reset, shutdown, standby, suspend;
+
+}
